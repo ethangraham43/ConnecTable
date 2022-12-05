e1415d48d1e12c5f20ba4a5968fffda9e7824d4a
Ethan Graham <ethangraham43@gmail.com>
2022-12-05 17:14:42 -0500

    added render-build.sh

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     resources :users, only: :create
     resource :session, only: [:show, :create, :destroy]
   end
+  get '*path', to: "static_pages#frontend_index"
 end
 
-get '*path', to: "static_pages#frontend_index"
+
