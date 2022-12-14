02d23e99c0137e0c8ae0257f4a8e257caa7ed52d
Ethan Graham <ethangraham43@gmail.com>
2022-12-14 09:50:22 -0500

    changed logo

--- frontend/src/store/reservations.js
+++ frontend/src/store/reservations.js
@@ -1,32 +1,41 @@
-export const RECEIVE_RESERVATION = 'RECEIVE_RESERVATION'
-export const RECEIVE_RESERVATIONS = 'RECEIVE_RESERVATIONS'
-export const REMOVE_RESERVATION = 'REMOVE_RESERVATION'
+// import csrfFetch from "./csrf"
 
-export const receiveReservations = reservations => ({
-    type: 'RECEIVE_RESERVATIONS',
-    reservations
-})
+// export const RECEIVE_RESERVATION = 'RECEIVE_RESERVATION'
+// export const RECEIVE_RESERVATIONS = 'RECEIVE_RESERVATIONS'
+// export const REMOVE_RESERVATION = 'REMOVE_RESERVATION'
 
-export const receiveReservation = reservation => ({
-    type: 'RECEIVE_RESERVATION',
-    reservation
-})
+// export const receiveReservations = reservations => ({
+//     type: 'RECEIVE_RESERVATIONS',
+//     reservations
+// })
 
-export const removeReservation = reservationId => ({
-    type: 'REMOVE_RESERVATION',
-    reservationId
-})
+// export const receiveReservation = reservation => ({
+//     type: 'RECEIVE_RESERVATION',
+//     reservation
+// })
 
-export const createReservation = reservation => async dispatch => {
-    const response = await csrfFetch("/api/reservations", {
-        method: "POST",
-        body: JSON.stringify(reservation)
-      });
-      const data = await response.json();
-    }
+// export const removeReservation = reservationId => ({
+//     type: 'REMOVE_RESERVATION',
+//     reservationId
+// })
 
-    function reservationsReducer(state={}, action) {
+// export const createReservation = reservation => async dispatch => {
+//     const response = await csrfFetch("/api/reservations", {
+//         method: "POST",
+//         body: JSON.stringify(reservation)
+//       });
+//       const data = await response.json();
+//     }
 
-    }
+//     function reservationsReducer(state={}, action) {
+//         const newState = { ...state };
+//         switch (action.type) {
+//             case RECEIVE_RESERVATIONS:
+//                 return action.reservations;
+//             case RECEIVE_RESERVATION:
 
-    export default reservationsReducer;
+
+//         }
+//     }
+
+//     export default reservationsReducer;
