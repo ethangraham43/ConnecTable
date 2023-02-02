6e584dbf60017f4817898c8bb9c8250d7cf45666
ethangraham43 <ethangraham43@gmail.com>
2023-02-02 16:11:34 -0500

    already imported react

--- frontend/src/components/Carousel/Carousel.js
+++ frontend/src/components/Carousel/Carousel.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./Carousel.css";
-import React from 'react';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import "pure-react-carousel/dist/react-carousel.es.css";
 
